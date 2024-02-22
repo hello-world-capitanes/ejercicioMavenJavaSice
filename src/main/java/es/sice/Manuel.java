74c699ba97f1ee3eb39160f56d02a51d982b51de
Tena Calero <mtena@sice-outsourcing.com>
2024-02-22 13:11:31 +0100

    Dependencia y creacion de la clase Manuel

---
+++ src/main/java/es/sice/Manuel.java
@@ -0,0 +1,12 @@
+package es.sice;
+
+public class Manuel {
+
+    private String Nombre;
+    private String Apellido;
+
+    public Manuel(){
+        this.Nombre = "Manuel";
+        this.Apellido = "Tena";
+    }
+}
